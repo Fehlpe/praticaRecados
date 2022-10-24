3e3ca9fb0a46a591ba5136b06f48096456217860
Sheila Acunha <johndoe@example.comsheilaacunha1990@gmail.com>
2022-10-24 18:17:23 -0300

    img sem fundo

--- src/Shared/components/erroPagina/ErroPagina.tsx
+++ src/Shared/components/erroPagina/ErroPagina.tsx
@@ -1,6 +1,7 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import styled from "styled-components";
-import imagem from "./img.jpg";
+import imagem from "./img.png";
+import { Link } from "react-router-dom";
 
 const DivImg = styled.div`
   padding: 10px;
@@ -9,26 +10,29 @@ const DivImg = styled.div`
 
   img {
     height: 500px;
+    transition: transform 0.3s;
+  }
+  img:hover {
+    transform: scale(1.2);
   }
 `;
 
 export default function ErroPagina() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid>
-        <Typography variant="h3" align="center" mt={5} p={2}>
-          Ops! Não encontramos essa página.
-        </Typography>
+      <Link to="/">Home</Link>
+      <Typography variant="h3" align="center" mt={5} p={2}>
+        Ops! Não encontramos essa página.
+      </Typography>
 
-        <Typography variant="body2" color="text.secondary" align="center">
-          Acho que você escolheu a página errada, porque eu não consegui achar a
-          que você está procurando.
-        </Typography>
-        <DivImg>
-          {" "}
-          <img src={imagem} alt="" />
-        </DivImg>
-      </Grid>
+      <Typography variant="body2" color="text.secondary" align="center">
+        Acho que você escolheu a página errada, porque eu não consegui achar a
+        que você está procurando.
+      </Typography>
+      <DivImg>
+        {" "}
+        <img src={imagem} alt="" />
+      </DivImg>
     </Box>
   );
 }
